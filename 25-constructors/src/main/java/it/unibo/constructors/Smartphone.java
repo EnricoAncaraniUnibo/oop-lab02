a7baae7404fabf6f215d3aa6b6cd3bb89b96e33a
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-12 10:30:43 +0100

    2.8

--- 25-constructors/src/main/java/it/unibo/constructors/Smartphone.java
+++ 25-constructors/src/main/java/it/unibo/constructors/Smartphone.java
@@ -18,6 +18,54 @@ class Smartphone {
     boolean has3G;
     boolean hasNFC;
 
+    public Smartphone() {
+        this.has3G=DEF_HAS_3G;
+        this.hasGPS=DEF_HAS_GPS;
+        this.hasNFC=DEF_HAS_NFC;
+        this.sdSize=DEF_SD_SIZE;
+        this.ram=DEF_RAM_SIZE;
+        this.nCPU=DEF_N_CPU;
+    }
+
+    Smartphone(String brand, String model, int nCPU, int ram, int sdSize, boolean hasGPS, boolean has3G, boolean hasNFC) {
+        this(brand, model, sdSize);
+        this.nCPU=nCPU;
+        this.ram=ram;
+        this.hasGPS=hasGPS;
+        this.has3G=has3G;
+        this.hasNFC=hasNFC;
+    }
+
+    Smartphone(String brand, String model, int nCPU, int ram, boolean hasGPS, boolean has3G, boolean hasNFC) {
+        this(brand, model);
+        this.nCPU=nCPU;
+        this.ram=ram;
+        this.hasGPS=hasGPS;
+        this.has3G=has3G;
+        this.hasNFC=hasNFC;
+    }
+
+    Smartphone(String brand, String model) {
+        this.brand=brand;
+        this.model=model;
+    }
+    
+    Smartphone(String brand, String model, boolean hasGPS, boolean has3g) {
+        this(brand, model);
+        this.hasGPS=hasGPS;
+        this.has3G=has3g;
+    }
+    
+    Smartphone(String brand, String model, int sdSize) {
+        this(brand, model);
+        this.sdSize=sdSize;
+    }
+    
+    Smartphone(String brand, String model, boolean hasNFC) {
+        this(brand, model);
+        this.hasNFC=hasNFC;
+    }
+
     void printStringRep() {
         System.out.println("Smartphone info:");
         System.out.println("n CPU(s): " + this.nCPU);
@@ -32,15 +80,22 @@ class Smartphone {
 
     public static void main(final String[] args) {
         // 1) Creare lo smarthpone HTC One sdSize:1024
-
+        Smartphone s1 = new Smartphone("HTC", "One", 1024);
+        s1.printStringRep();
         // 2) Creare lo smarthpone Samsung Galaxy Note 3 ram:2048 cpu:4
         // sdSize:8192 gps:true nfc:true 3g:true
-
+        Smartphone s2 = new Smartphone("Samsung", "Galaxy", 4, 2048 , false,false,false);
         // 3) Creare lo smarthpone Apple iPhone 5S nfc:false
-
+        Smartphone s3 = new Smartphone("Apple iphone", "5s", false);
         // 4) Creare lo smarthpone Google Nexus 4 gps:true 3g:true
-
+        Smartphone s4 = new Smartphone("Google Nexus", "4", true,true);
         // 5) Utilizzare il metodo printStringRep per stampare in standard
+        Smartphone s5=new Smartphone();
         // output le informazioni di ciascun telefono
+        s1.printStringRep();
+        s2.printStringRep();
+        s3.printStringRep();
+        s4.printStringRep();
+        s5.printStringRep();
     }
 }
